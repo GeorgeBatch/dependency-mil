9d8c63a62f54ffec6d6a3c58cbf0309ea8e37082
George Batchkala <george.batchkala@gmail.com>
2025-02-07 09:17:17 +0000

    more constants

--- source/constants.py
+++ source/constants.py
@@ -4,3 +4,22 @@ PROJECT_PATH = osp.abspath(osp.join(osp.dirname(osp.realpath(__file__)), '../'))
 
 CONFIGS_PATH = osp.join(PROJECT_PATH, 'configs')
 EXPERIMENTS_PATH = osp.join(PROJECT_PATH, 'experiments')
+
+EXTRACTOR_NAMES_2_WEIGHTS_PATHS = {
+    'simclr-tcga-lung_resnet18-10x': osp.join(
+        PROJECT_PATH, 'weights-pre-trained/simclr-tcga-lung/weights-10x/model-v1.pth'),
+    'simclr-tcga-lung_resnet18-2.5x': osp.join(
+        PROJECT_PATH, 'weights-pre-trained/simclr-tcga-lung/weights-2.5x/model-v1.pth'),
+    'simclr-camelyon16_resnet18-20x': osp.join(
+        PROJECT_PATH, 'weights-pre-trained/simclr-camelyon16/weights-20x/model-v2.pth'),
+    'simclr-camelyon16_resnet18-5x': osp.join(
+        PROJECT_PATH, 'weights-pre-trained/simclr-camelyon16/weights-5x/model.pth'),
+}
+
+DATASET_SPECIFIC_NORMALIZATION_CONSTANTS_PATH = osp.join(PROJECT_PATH, 'source/feature_extraction/img_normalisation_constants.json')
+ALL_IMG_NORMS = (
+    'imagenet',
+    'openai_clip',
+    'uniform',
+    'resize_only',
+)
